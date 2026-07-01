2cdcb18e69baa56cabda968ae6888841bcb81548
Xenon Zoidberg <xoz@zoidberg.tech>
2026-06-30 21:53:42 -0700

    Created landing page sans logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  devToolbar: { enabled: false },
   site: "https://zoidberg.tech",
   vite: {
     plugins: [tailwindcss()],
